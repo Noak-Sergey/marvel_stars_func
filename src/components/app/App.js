03a0d35937fc94de62e3ef214a39a4f4f52ef6c7
Noak-Sergey <noakstuff@gmail.com>
2022-08-31 10:28:15 +0300

    first commit

---
+++ src/components/app/App.js
@@ -0,0 +1,44 @@
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import AppHeader from "../appHeader/AppHeader";
+import Spinner from '../spinner/Spinner';
+
+const Page404 = lazy(() => import('../pages/404'));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/singleComicPage/SingleComicPage'));
+const SingleCharacterPage = lazy(() => import('../pages/singleCharacterPage/SingleCharacterPage'));
+const SinglePage = lazy(() => import('../pages/SinglePage'));
+
+const App = () => {
+
+    
+    return (
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Suspense fallback={<Spinner/>}>
+                        <Routes>
+
+                            <Route path="/" element={<MainPage/>}/>
+                            
+                            <Route path="/comics" element={<ComicsPage/>}/>
+
+                            {/* <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
+                            <Route path="/characters/:id" element={<SingleCharacterPage/>}/> */}
+                            <Route path="/comics/:comicId" element={<SinglePage Component={SingleComicPage} dataType="comic"/>}/>
+                            <Route path="/characters/:id" element={<SinglePage Component={SingleCharacterPage} dataType="character"/>}/>
+
+                            <Route path="*" element={<Page404/>}/>
+
+                        </Routes>
+                    </Suspense>
+                </main>
+            </div>
+        </Router>
+    )
+}
+
+export default App;
---
+++ src/services/MarvelService.js
@@ -0,0 +1,62 @@
+import { useHttp } from "../hooks/http.hook";
+
+const useMarvelService = () => {
+    const {loading, request, error, clearError} = useHttp();
+
+    const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    const _apiKey = 'apikey=2237c04bac4b813ebf6c1cb1ee2c41d9';
+    const _baseOffset = 210;
+
+    const getAllCharacters = async (offset = _baseOffset) => {
+        const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
+        return res.data.results.map(_transformCharacter) //при такой записи вызова функции аргумент будет подставляться автоматически
+    }
+
+    const getCharacterByName = async (name) => {
+        const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
+        return res.data.results.map(_transformCharacter);
+    }
+
+    const getCharacter = async (id) => {
+        const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
+        return _transformCharacter(res.data.results[0]);
+    }
+
+    const getAllComics = async (offset = 0) => {
+        const res = await request(`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`);
+        return res.data.results.map(_transformComics);
+    }
+
+    const getComic = async (id) => {
+        const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+        return _transformComics(res.data.results[0]);
+    }
+
+    const _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : `There is no description for this character`,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+            id: char.id,
+            comics: char.comics.items
+        }
+    }
+
+    const _transformComics = (comics) => {
+        return {
+            id: comics.id,
+            title: comics.title,
+            description: comics.description || 'There is no description',
+            pageCount: comics.pageCount ? `${comics.pageCount} p.` : 'No information about the number of pages',
+            thumbnail: comics.thumbnail.path + '.' + comics.thumbnail.extension,
+            language: comics.textObjects.language || 'en-us',
+            price: comics.prices.price ? `${comics.prices.price}$` : 'not available'
+        }
+    }
+
+    return {loading, error, clearError, getAllCharacters, getCharacterByName, getCharacter, getAllComics, getComic}
+}
+
+export default useMarvelService;
